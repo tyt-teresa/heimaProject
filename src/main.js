ae431ba86c37fa4cf9fd33b7feb5ec9f357e4f17
tyt-teresa <1341105528@qq.com>
2019-08-19 21:01:51 +0800

    引入elementUI

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import elementUI from 'element-ui'
 
 Vue.config.productionTip = false
-
+Vue.use(elementUI)
 new Vue({
   router,
   render: h => h(App)
