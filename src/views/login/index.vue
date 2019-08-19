9140a755abe4c42e401fa5b4b1e4790dfd0633bd
tyt-teresa <1341105528@qq.com>
2019-08-19 23:50:32 +0800

    登录页数据绑定及校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,19 +4,19 @@
       <div class="title">
         <img src="../../assets/image/logo_index.png" alt="黑马头条">
       </div>
-      <el-form>
-        <el-form-item>
-          <el-input placeholder="请输入手机号"></el-input>
+      <el-form :model="ruleForm" ref="form" :rules="rules">
+        <el-form-item prop="mobile">
+          <el-input placeholder="请输入手机号" v-model="ruleForm.mobile"></el-input>
         </el-form-item>
-        <el-form-item>
-          <el-input placeholder="请输入验证码" style="width:60%"></el-input>
+        <el-form-item prop="code">
+          <el-input v-model="ruleForm.code" placeholder="请输入验证码" style="width:60%"></el-input>
           <el-button style="width:34%;float:right">发送验证码</el-button>
         </el-form-item>
-        <el-form-item>
-           <el-checkbox v-model="checked">我已阅读并同意<a style="color:#409eff">用户协议</a>和<a style="color:#409eff">隐私条款</a></el-checkbox>
+        <el-form-item prop="checked">
+           <el-checkbox v-model="ruleForm.checked">我已阅读并同意<a style="color:#409eff">用户协议</a>和<a style="color:#409eff">隐私条款</a></el-checkbox>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" style="width:100%">登录</el-button>
+          <el-button type="primary" style="width:100%" @click="login">登录</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -25,7 +25,66 @@
 
 <script>
 export default {
+  data () {
+    var fn = function (rule, value, callback) {
+      if (value) {
+        callback()
+      } else {
+        callback(new Error('请勾选协议'))
+      }
+    }
+    return {
+      ruleForm: {
+        mobile: '',
+        code: '',
+        checked: false
+      },
+      rules: {
+        mobile: [
+          {
+            required: true,
+            message: '手机号不能为空',
+            tigger: 'blur'
+          },
+          {
+            len: 11,
+            message: '手机号长度必须为11个字符',
+            tigger: 'blur'
+          },
+          {
+            pattern: /^1[3456789]\d{9}$/,
+            message: '手机号格式错误',
+            tigger: 'blur'
+          }
+        ],
+        code: [
+          {
+            required: true,
+            message: '验证码不能为空'
+          },
+          {
+            pattern: /^\d{6}$/,
+            message: '验证码长度必须为6个数字',
+            tigger: 'blur'
+          }
+        ],
+        checked: [
+          {
+            validator: fn
+          }
+        ]
+      }
+    }
+  },
+  methods: {
+    login () {
+      this.$refs.form.validate(isOK => {
+        if (isOK) {
 
+        }
+      })
+    }
+  }
 }
 </script>
 
