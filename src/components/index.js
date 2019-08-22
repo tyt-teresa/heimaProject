844c8c4fefc897c7fa87262968d124a8d4ecc236
tyt-teresa <1341105528@qq.com>
2019-08-23 00:47:56 +0800

    面包屑组件封装

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,10 @@
 import homeAside from './home/home-aside.vue'
 import homeHeader from './home/home-header.vue'
+import breadCrumb from './common/breadcrumb.vue'
 export default {
   install (Vue) {
     Vue.component('home-aside', homeAside)
     Vue.component('home-header', homeHeader)
+    Vue.component('bread-crumb', breadCrumb)
   }
 }
