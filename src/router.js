8b5cf47c9d22ce7b59673409f2d381c7343ac1fd
tyt-teresa <1341105528@qq.com>
2019-08-23 21:51:33 +0800

    新建素材列表并挂载路由

--- src/router.js
+++ src/router.js
@@ -26,6 +26,9 @@ export default new Router({
         }, {
           path: '/home/comment',
           component: () => import('./views/comment')
+        }, {
+          path: '/home/material',
+          component: () => import('./views/material')
         }
       ]
     }
---
+++ src/views/material/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <el-card>
+    <bread-crumb slot="header">
+      <template slot="title">素材列表</template>
+    </bread-crumb>
+    <div>素材列表</div>
+  </el-card>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
