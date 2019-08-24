f69b4e95fe4394bcd288cd1cdda81f95ccf6d484
tyt-teresa <1341105528@qq.com>
2019-08-24 11:55:16 +0800

    内容列表-页面结构-搜索工具栏

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -3,11 +3,65 @@
     <bread-crumb slot="header">
       <template slot="title">内容列表</template>
     </bread-crumb>
+    <el-form>
+      <el-form-item label="文章状态:">
+        <el-radio-group v-model="radio">
+          <el-radio :label="5">全部</el-radio>
+          <el-radio :label="0">草稿</el-radio>
+          <el-radio :label="1">待审核</el-radio>
+          <el-radio :label="2">审核通过</el-radio>
+          <el-radio :label="3">审核失败</el-radio>
+        </el-radio-group>
+      </el-form-item>
+      <el-form-item label="频道列表:">
+        <el-select v-model="value" placeholder="请选择">
+          <el-option v-for="item in list" :key="item.value" :label="item.label" :value="item.value"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="时间选择:">
+        <el-date-picker
+          v-model="value1"
+          type="daterange"
+          start-placeholder="开始日期"
+          end-placeholder="结束日期"
+        ></el-date-picker>
+      </el-form-item>
+    </el-form>
   </el-card>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      radio: 5,
+      list: [
+        {
+          value: '选项1',
+          label: '黄金糕'
+        },
+        {
+          value: '选项2',
+          label: '双皮奶'
+        },
+        {
+          value: '选项3',
+          label: '蚵仔煎'
+        },
+        {
+          value: '选项4',
+          label: '龙须面'
+        },
+        {
+          value: '选项5',
+          label: '北京烤鸭'
+        }
+      ],
+      value: '',
+      value1: ''
+    }
+  }
+}
 </script>
 
 <style>
