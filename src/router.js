db04dedfd3c4a63493b28cb8854b7dceac381c7c
tyt-teresa <1341105528@qq.com>
2019-08-24 19:05:39 +0800

    发表文章页面结构及挂载路由实现

--- src/router.js
+++ src/router.js
@@ -32,6 +32,9 @@ export default new Router({
         }, {
           path: '/home/articles',
           component: () => import('./views/articles')
+        }, {
+          path: '/home/publish',
+          component: () => import('./views/publish')
         }
       ]
     }
