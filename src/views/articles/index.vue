ae9c6ff6c5ee115812f885d142f2dbe1beff4f20
tyt-teresa <1341105528@qq.com>
2019-08-24 15:29:21 +0800

    内容列表-页面结构-请求数据

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -3,7 +3,7 @@
     <bread-crumb slot="header">
       <template slot="title">内容列表</template>
     </bread-crumb>
-    <el-form>
+    <el-form class="total-title">
       <el-form-item label="文章状态:">
         <el-radio-group v-model="radio">
           <el-radio :label="5">全部</el-radio>
@@ -15,7 +15,7 @@
       </el-form-item>
       <el-form-item label="频道列表:">
         <el-select v-model="value" placeholder="请选择">
-          <el-option v-for="item in list" :key="item.value" :label="item.label" :value="item.value"></el-option>
+          <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value"></el-option>
         </el-select>
       </el-form-item>
       <el-form-item label="时间选择:">
@@ -27,6 +27,35 @@
         ></el-date-picker>
       </el-form-item>
     </el-form>
+    <div class="total_title">共找到条符合条件的内容</div>
+    <!-- 内容列表 -->
+    <div class="content-list">
+      <!-- 循环项 -->
+      <div class="content-item" v-for="(item,index) in list" :key="index">
+        <!-- 左侧内容 -->
+        <div class="left">
+          <img src="../../assets/image/collect.png" alt />
+          <!-- 内容信息 -->
+          <div class="info">
+            <!-- 文章标题 -->
+            <span></span>
+            <el-tag style="width:60px">已发表</el-tag>
+            <span class="date"></span>
+          </div>
+        </div>
+        <!-- 右侧内容 -->
+        <div class="right">
+          <span style='cursor:pointer'>
+            <i class="el-icon-edit"></i>
+            修改
+          </span>
+          <span @click="delItem(item)" style='cursor:pointer'>
+            <i class="el-icon-delete"></i>
+            删除
+          </span>
+        </div>
+      </div>
+    </div>
   </el-card>
 </template>
 
@@ -35,7 +64,7 @@ export default {
   data () {
     return {
       radio: 5,
-      list: [
+      options: [
         {
           value: '选项1',
           label: '黄金糕'
@@ -58,11 +87,55 @@ export default {
         }
       ],
       value: '',
-      value1: ''
+      value1: '',
+      list: []
     }
   }
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+.total-title {
+  border-bottom: 1px dashed #ccc;
+  padding-left: 40px;
+}
+.content-list {
+  .content-item {
+    padding: 10px;
+    border-bottom: 1px solid #f2f3f5;
+    display: flex;
+    justify-content: space-between;
+    .left {
+      display: flex;
+      align-items: center;
+      img {
+        width: 150px;
+        height: 100px;
+        border-radius: 4px;
+      }
+      .info {
+        margin-left: 10px;
+        display: flex;
+        height: 100px;
+        padding: 5px 0;
+        flex-direction: column;
+        justify-content: space-between;
+        .date {
+          color: #999;
+          font-size: 12px;
+        }
+      }
+    }
+    .right {
+      span,
+      span i {
+        font-size: 12px;
+        color: #333;
+      }
+      span {
+        margin-right: 5px;
+      }
+    }
+  }
+}
 </style>
