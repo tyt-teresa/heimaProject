094bc6e1facd52195bf49070c0100c6da66d52f3
tyt-teresa <1341105528@qq.com>
2019-08-25 08:28:40 +0800

    发表文章-发布文章检验逻辑实现

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -3,14 +3,14 @@
     <bread-crumb slot="header">
       <template slot="title">发布文章</template>
     </bread-crumb>
-    <el-form label-width="80px">
-      <el-form-item label="标题">
+    <el-form label-width="80px" :model="formData" :rules="rules" ref="myFrom">
+      <el-form-item label="标题" prop="title">
         <el-input style="width:400px"></el-input>
-      </el-form-item>
-      <el-form-item label="内容">
+      </el-form-item >
+      <el-form-item label="内容" prop="content">
         <el-input type="textarea" placeholder="请输入内容" :rows="20"></el-input>
       </el-form-item>
-      <el-form-item label="封面">
+      <el-form-item label="封面" prop="cover">
         <el-radio-group>
           <el-radio>单图</el-radio>
           <el-radio>三图</el-radio>
@@ -18,7 +18,7 @@
           <el-radio>自动</el-radio>
         </el-radio-group>
       </el-form-item>
-      <el-form-item label="频道">
+      <el-form-item label="频道" prop="channel_id">
         <el-select placeholder="请选择">
           <el-option
             v-for="item in channels"
@@ -29,7 +29,7 @@
         </el-select>
       </el-form-item>
       <el-form-item label>
-          <el-button type="primary">发表</el-button>
+          <el-button type="primary" @click="publish">发表</el-button>
            <el-button>存为草稿</el-button>
       </el-form-item>
     </el-form>
@@ -40,10 +40,28 @@
 export default {
   data () {
     return {
-      channels: []
+      channels: [],
+      formData: {
+        title: '',
+        content: '',
+        cover: 0,
+        channel_id: null
+      },
+      rules: {
+        title: [{ required: true, message: '标题不能为空' }],
+        content: [{ required: true, message: '内容不能为空' }],
+        channel_id: [{ required: true, message: '频道不能为空' }]
+      }
     }
   },
   methods: {
+    publish () {
+      this.$refs.myFrom.validate((isOK) => {
+        if (isOK) {
+
+        }
+      })
+    },
     getChannels () {
       this.$axios({
         url: '/channels'
