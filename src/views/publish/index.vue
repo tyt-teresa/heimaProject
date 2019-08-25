e368a527b908f2da6712777137fe16ccce8c2515
tyt-teresa <1341105528@qq.com>
2019-08-25 19:16:55 +0800

    发表文章-发布文章

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -5,21 +5,21 @@
     </bread-crumb>
     <el-form label-width="80px" :model="formData" :rules="rules" ref="myFrom">
       <el-form-item label="标题" prop="title">
-        <el-input style="width:400px"></el-input>
+        <el-input style="width:400px" v-model="formData.title"></el-input>
       </el-form-item >
       <el-form-item label="内容" prop="content">
-        <el-input type="textarea" placeholder="请输入内容" :rows="20"></el-input>
+        <el-input type="textarea" placeholder="请输入内容" :rows="20" v-model="formData.content"></el-input>
       </el-form-item>
       <el-form-item label="封面" prop="cover">
-        <el-radio-group>
-          <el-radio>单图</el-radio>
-          <el-radio>三图</el-radio>
-          <el-radio>无图</el-radio>
-          <el-radio>自动</el-radio>
+        <el-radio-group v-model="formData.cover.type">
+          <el-radio :label="1">单图</el-radio>
+          <el-radio :label="3">三图</el-radio>
+          <el-radio :label="0">无图</el-radio>
+          <el-radio :label="-1">自动</el-radio>
         </el-radio-group>
       </el-form-item>
       <el-form-item label="频道" prop="channel_id">
-        <el-select placeholder="请选择">
+        <el-select placeholder="请选择" v-model="formData.channel_id">
           <el-option
             v-for="item in channels"
             :key="item.id"
@@ -29,8 +29,8 @@
         </el-select>
       </el-form-item>
       <el-form-item label>
-          <el-button type="primary" @click="publish">发表</el-button>
-           <el-button>存为草稿</el-button>
+          <el-button type="primary" @click="publish(false)">发表</el-button>
+           <el-button @click="publish(true)">存为草稿</el-button>
       </el-form-item>
     </el-form>
   </el-card>
@@ -41,24 +41,41 @@ export default {
   data () {
     return {
       channels: [],
+      draft: false,
       formData: {
         title: '',
         content: '',
-        cover: 0,
+        cover: {
+          type: 0,
+          images: []
+        },
         channel_id: null
       },
       rules: {
-        title: [{ required: true, message: '标题不能为空' }],
+        title: [{ required: true, message: '标题不能为空' },
+          {
+            min: 5,
+            max: 30,
+            message: '标题内容需要在5~30字符之间'
+          }],
         content: [{ required: true, message: '内容不能为空' }],
         channel_id: [{ required: true, message: '频道不能为空' }]
       }
     }
   },
   methods: {
-    publish () {
+    publish (draft) {
       this.$refs.myFrom.validate((isOK) => {
         if (isOK) {
-
+          this.$axios({
+            method: 'post',
+            url: '/articles',
+            params: { draft },
+            data: this.formData
+          }).then(result => {
+            // console.log(result)
+            this.$router.push('/home/articles')
+          })
         }
       })
     },
